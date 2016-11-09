5850b1a21a71b0c65e14a5cf6b1be003385221a3
Sinney Chan <schan@mail.lipscomb.edu>
2016-11-08 19:45:14 -0700

    famous paintings

---
+++ FamousPainting.java
@@ -0,0 +1,11 @@
+/**
+ * Created by sing_ on 11/8/2016.
+ */
+    class FamousPainting extends Painting {
+
+    public FamousPainting(String t, String a){
+        super(t,a);
+        value = 25000;
+    }
+
+}
